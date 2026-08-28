9a405325a24cd7fcdd0318e4f3a801880fb7f28a
Pierre-Alexis Valbrun <pierre-alexis.valbrun@epita.fr>
2026-08-29 00:18:03 +0200

    fix(docker): expose notify:discord script in production build and root package.json

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/scripts/send-discord-notification.ts"],
   clean: true,
   format: ["esm"],
   platform: "node",
